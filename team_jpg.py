8f5312c4a35017a7bf0979e2a345e9d147882bdb
fcvbnjtyfgh9 <akdldkdlel13@gmail.com>
2023-12-13 22:12:48 +0900

    main sorces

--- team_jpg.py
+++ team_jpg.py
@@ -2,47 +2,8 @@ import cv2
 import numpy as np
 import pytesseract
 
-def detect_traffic_lights(image_path):
-
-    image = cv2.imread(image_path)
-    
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=200, param2=35, minRadius=10, maxRadius=40)
-    
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        
-        for i in circles[0, :]:
-    
-            cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            
-        
-            roi = hsv_image[i[1]-i[2]:i[1]+i[2], i[0]-i[2]:i[0]+i[2]]
-            mean_color = cv2.mean(roi)
-            
-            # 색상 판별
-            if mean_color[0] > 0 and mean_color[0] < 20: 
-                cv2.putText(image, 'Red', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            elif mean_color[0] > 75 and mean_color[0] < 80:  
-                cv2.putText(image, 'Green', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    
-    cv2.imshow('Detected Traffic Lights', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-image_path = r'C:\Users\user\image\test3.png'
-detect_traffic_lights(image_path)
-
-# 도로의 초록색 표지판을 기준으로 작성
-# 초록색 사각형 표지판 인식 시 출력되는 이미지에 표지판을 표시하고, 'Square Sign Found' 출력
-# 이미지를 로드하지 못할 경우, 콘솔창에 'Fail to load image.' 출력
-# 사각형 표지판만 인식하기 위해 일정 크기 이상의 사각형만 표시
+# Tesseract OCR 경로 설정 (설치된 경로에 맞게 수정하세요)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\user\tesseract\tesseract.exe'
 
 def find_square_sign(image, min_contour_length=100):
     # 이미지를 HSV 색 공간으로 변환
@@ -77,71 +38,65 @@ def find_square_sign(image, min_contour_length=100):
     # 네모 모양의 표지판이 있는지 여부를 반환
     return len([contour for contour in contours if len(cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)) == 4 and cv2.arcLength(contour, True) >= min_contour_length]) > 0
 
-# 이미지 파일을 읽어옴
-image_path = './image/test6.webp'
-image = cv2.imread(image_path)
-
-if image is not None:
-    # 네모 모양의 표지판을 찾음 (최소 둘레 길이를 100으로 설정)
-    square_sign_found = find_square_sign(image, min_contour_length=95)
-
-    # 네모 모양의 표지판이 있으면 메시지를 표시
-    if square_sign_found:
-        cv2.putText(image, 'Square Sign Found', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    # 결과 이미지를 표시
-    cv2.imshow('Square Sign Detection', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("Failed to load image.")
-
-
-# Detect Text in Image
-# Tesseract OCR 경로 설정 (설치된 경로에 맞게 수정하세요)
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\user\tesseract\tesseract.exe'
-
-def detect_text(image_path):
-    # 이미지 불러오기
+def detect_text_traffic_lights_and_square_sign(image_path):
     image = cv2.imread(image_path)
 
-    if image is None:
-        print(f"Error: Unable to load the image at path {image_path}")
-        return "", None  # 이미지 로드 실패 시 빈 문자열과 None 반환
-
-    # 그레이스케일 변환
+    # Text detection
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # 이미지 전처리 (예: 가우시안 블러 적용)
     preprocessed_image = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    # 흰색 글씨 추출을 위한 이진화 (thresholding)
     _, binary_image = cv2.threshold(preprocessed_image, 180, 255, cv2.THRESH_BINARY_INV)
+    detected_text = pytesseract.image_to_string(binary_image, lang='eng')
 
-    # 텍스트 감지 (언어 설정 추가)
-    text = pytesseract.image_to_string(binary_image, lang='eng')
+    # Square sign detection
+    square_sign_found = find_square_sign(image, min_contour_length=95)
 
-    return text, image
+    # Traffic light detection
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=80,
+                               param1=181, param2=20, minRadius=10, maxRadius=40)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0, :]:
+            cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+
+            roi = hsv_image[i[1]-i[2]:i[1]+i[2], i[0]-i[2]:i[0]+i[2]]
+            mean_color = cv2.mean(roi)
+
+            # 색상 판별
+            if mean_color[0] > 105 and mean_color[0] < 120:
+                cv2.putText(image, 'Red', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            elif mean_color[0] > 80 and mean_color[0] < 90:
+                cv2.putText(image, 'Green', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+    # 결과 반환
+    return detected_text, image, square_sign_found
 
 def main():
-    # 표지판 이미지 경로 설정
-    image_path = r'C:\Users\user\image\test31.jp'
+    # 이미지 경로 설정
+    image_path = r'C:\Users\user\image\test7.jpg'
 
-    # 텍스트 감지
-    detected_text, image = detect_text(image_path)
+    # 텍스트, 교통 신호등, 그리고 네모 모양의 표지판 감지
+    detected_text, combined_image, square_sign_found = detect_text_traffic_lights_and_square_sign(image_path)
 
-    if detected_text and image is not None:
+    if detected_text and combined_image is not None:
         # 결과 출력
         print("Detected Text:")
         print(detected_text)
 
+        # 네모 모양의 표지판이 있으면 메시지를 표시
+        if square_sign_found:
+            cv2.putText(combined_image, 'Square Sign Found', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         # 이미지 표시
-        cv2.imshow('detect_text', image)
+        cv2.imshow('Detect Text, Traffic Lights, and Square Sign', combined_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     else:
-        print("Text detection failed.")
+        print("Detection failed.")
 
 if __name__ == "__main__":
     main()
-
